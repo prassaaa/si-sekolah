d65ee312ca673e6d75d477f647a7f89edf0d5adc
Prass <pras.ari69@gmail.com>
2026-01-17 00:45:52 +0700

    refactor: Remove unused FilamentInfoWidget from AuthPanelProvider

--- app/Providers/Filament/AuthPanelProvider.php
+++ app/Providers/Filament/AuthPanelProvider.php
@@ -12,7 +12,6 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -40,7 +39,6 @@ class AuthPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
                 AccountWidget::class,
-                FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
