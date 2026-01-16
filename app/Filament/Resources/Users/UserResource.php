689da9d858431bb305f22401a9e8e62523c0dbfe
Prass <pras.ari69@gmail.com>
2026-01-16 21:21:15 +0700

    fix: update navigation icon for UserResource to use OutlinedUsers

--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -21,7 +21,7 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedUsers;
 
     protected static ?string $navigationLabel = 'Pengguna';
 
