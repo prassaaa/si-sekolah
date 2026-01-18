9fec67af979124a894cf9514d13fda7a397e2121
Prass <pras.ari69@gmail.com>
2026-01-18 10:49:19 +0700

    fix: Update import statements to use Schema for KategoriPembayaranForm and KelasForm

--- app/Filament/Resources/KategoriPembayarans/Schemas/KategoriPembayaranForm.php
+++ app/Filament/Resources/KategoriPembayarans/Schemas/KategoriPembayaranForm.php
@@ -3,10 +3,10 @@
 namespace App\Filament\Resources\KategoriPembayarans\Schemas;
 
 use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class KategoriPembayaranForm
