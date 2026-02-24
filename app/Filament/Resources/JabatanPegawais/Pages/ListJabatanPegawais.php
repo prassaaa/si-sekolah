8e23d0821df1145dff172297cb783f309fed9325
Prass <pras.ari69@gmail.com>
2026-02-24 19:34:25 +0700

    feat(ListJabatanPegawais): Add header action for creating new JabatanPegawai

--- app/Filament/Resources/JabatanPegawais/Pages/ListJabatanPegawais.php
+++ app/Filament/Resources/JabatanPegawais/Pages/ListJabatanPegawais.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\JabatanPegawais\Pages;
 
 use App\Filament\Resources\JabatanPegawais\JabatanPegawaiResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListJabatanPegawais extends ListRecords
 {
     protected static string $resource = JabatanPegawaiResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
