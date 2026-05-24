b937c4b8ee83d211bd91b50ca28c7cca5a9c9d31
Prass <pras.ari69@gmail.com>
2026-05-24 21:12:33 +0700

    feat(ListInformasis, ListSekolahs): Add create action to header for listing pages

--- app/Filament/Resources/Sekolahs/Pages/ListSekolahs.php
+++ app/Filament/Resources/Sekolahs/Pages/ListSekolahs.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\Sekolahs\Pages;
 
 use App\Filament\Resources\Sekolahs\SekolahResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListSekolahs extends ListRecords
 {
     protected static string $resource = SekolahResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
